966becb0c6472eff76deb6a2ecdbf0e659eb0ee4
jeonyeonkyu <jyk6915@gmail.com>
2022-03-30 04:07:39 +0900

    refactor: performances에 amount프로퍼티를 추가해줌으로써 amountFor함수 중간데이터로 분리

--- ch1/statement.js
+++ ch1/statement.js
@@ -4,6 +4,7 @@ const statement = (invoice, plays) => {
   const enrichPerformance = (aPerformance) => {
     const result = Object.assign({}, aPerformance)
     result.play = playFor(result)
+    result.amount = amountFor(result)
     return result
   }
 
@@ -19,28 +20,6 @@ const statement = (invoice, plays) => {
 }
 
 const renderPainText = (data) => {
-  const amountFor = (aPerformance) => {
-    let result = 0
-    switch (aPerformance.play.type) {
-      case 'tragedy': {
-        result = 40000
-        if (aPerformance.audience > 30)
-          result += 1000 * (aPerformance.audience - 30)
-        break
-      }
-      case 'comedy': {
-        result = 30000
-        if (aPerformance.audience > 20)
-          result += 10000 + 500 * (aPerformance.audience - 20)
-        result += 300 * aPerformance.audience
-        break
-      }
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
-    }
-    return result
-  }
-
   const volumeCreditsFor = (aPerformance) => {
     let result = 0
     result += Math.max(aPerformance.audience - 30, 0)
@@ -69,14 +48,14 @@ const renderPainText = (data) => {
   const getTotalAmount = () => {
     let result = 0
     for (const aPerformance of data.performances) {
-      result += amountFor(aPerformance)
+      result += aPerformance.amount
     }
     return result
   }
 
   let result = `청구 내역 (고객명: ${data.customer})\n`
   for (const aPerformance of data.performances) {
-    result += `  ${aPerformance.play.name}: ${usd(amountFor(aPerformance))} (${
+    result += `  ${aPerformance.play.name}: ${usd(aPerformance.amount)} (${
       aPerformance.audience
     }석)\n`
   }
@@ -86,4 +65,25 @@ const renderPainText = (data) => {
   return result
 }
 
+const amountFor = (aPerformance) => {
+  let result = 0
+  switch (aPerformance.play.type) {
+    case 'tragedy': {
+      result = 40000
+      if (aPerformance.audience > 30)
+        result += 1000 * (aPerformance.audience - 30)
+      break
+    }
+    case 'comedy': {
+      result = 30000
+      if (aPerformance.audience > 20)
+        result += 10000 + 500 * (aPerformance.audience - 20)
+      result += 300 * aPerformance.audience
+      break
+    }
+    default:
+      throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
+  }
+  return result
+}
 export default statement

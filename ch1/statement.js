3a3f86d53255c58195536df061512e7208951dcc
jeonyeonkyu <jyk6915@gmail.com>
2022-03-30 04:17:07 +0900

    refactor: 함수 옮기기

--- ch1/statement.js
+++ ch1/statement.js
@@ -5,6 +5,7 @@ const statement = (invoice, plays) => {
     const result = Object.assign({}, aPerformance)
     result.play = playFor(result)
     result.amount = amountFor(result)
+    result.volumeCredits = volumeCreditsFor(result)
     return result
   }
 
@@ -12,45 +13,22 @@ const statement = (invoice, plays) => {
     return plays[aPerformance.playID]
   }
 
-  const statementData = {
-    customer: invoice.customer,
-    performances: invoice.performances.map(enrichPerformance),
-  }
-  return renderPainText(statementData, plays)
+  const statementData = {}
+  statementData.customer = invoice.customer
+  statementData.performances = invoice.performances.map(enrichPerformance)
+  statementData.totalAmount = getTotalAmount(statementData)
+  statementData.TotalVolumeCredits = getTotalVolumeCredits(statementData)
+
+  return renderPainText(statementData)
 }
 
 const renderPainText = (data) => {
-  const volumeCreditsFor = (aPerformance) => {
-    let result = 0
-    result += Math.max(aPerformance.audience - 30, 0)
-    if (aPerformance.play.type === 'comedy') {
-      result += Math.floor(aPerformance.audience / 5)
-    }
-    return result
-  }
-
   const usd = (aNumber) => {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
       minimumFractionDigits: 2,
     }).format(aNumber / 100)
-  }
-
-  const getTotalVolumeCredits = () => {
-    let result = 0 // 적립포인트
-    for (const aPerformance of data.performances) {
-      result += volumeCreditsFor(aPerformance)
-    }
-    return result
-  }
-
-  const getTotalAmount = () => {
-    let result = 0
-    for (const aPerformance of data.performances) {
-      result += aPerformance.amount
-    }
-    return result
   }
 
   let result = `청구 내역 (고객명: ${data.customer})\n`
@@ -60,8 +38,8 @@ const renderPainText = (data) => {
     }석)\n`
   }
 
-  result += `총액: ${usd(getTotalAmount())}\n`
-  result += `적립 포인트: ${getTotalVolumeCredits()}점\n`
+  result += `총액: ${usd(data.totalAmount)}\n`
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`
   return result
 }
 
@@ -86,4 +64,30 @@ const amountFor = (aPerformance) => {
   }
   return result
 }
+
+const volumeCreditsFor = (aPerformance) => {
+  let result = 0
+  result += Math.max(aPerformance.audience - 30, 0)
+  if (aPerformance.play.type === 'comedy') {
+    result += Math.floor(aPerformance.audience / 5)
+  }
+  return result
+}
+
+const getTotalVolumeCredits = (data) => {
+  let result = 0 // 적립포인트
+  for (const aPerformance of data.performances) {
+    result += aPerformance.volumeCredits
+  }
+  return result
+}
+
+const getTotalAmount = (data) => {
+  let result = 0
+  for (const aPerformance of data.performances) {
+    result += aPerformance.amount
+  }
+  return result
+}
+
 export default statement
